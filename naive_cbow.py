eb135e91cb4456772f388bb23e00436c01f0bf0d
ximua00 <33280006+ximua00@users.noreply.github.com>
2017-11-18 11:31:19 +0100

    Add files via upload

--- naive_cbow.py
+++ naive_cbow.py
@@ -15,17 +15,6 @@ train = list(data_naive_cbow(dict_file_train))
 dev = list(data_naive_cbow(dict_file_val))
 test = list(data_naive_cbow(dict_file_test))
 
-def make_matrix(text_features, image_features, embedding_dim, img_feature_size, output_dim):
-	#Helper function for forward propagation
-	#Image feature should be a list of tensors
-	"""Create a matrix for the forward propagation TEXT | IMAGE, for all images. """
-	matrix = torch.zeros(output_dim, embedding_dim + img_feature_size)
-	i = 0
-	for image in image_features:
-		data = torch.cat((text_features,image))
-		matrix[i,:] = data
-		i += 1
-	return Variable(matrix)
 
 
 def evaluate(model, data):
@@ -44,11 +33,11 @@ def evaluate(model, data):
 
 		if predict == correct_index:
 			TOP1 += 1
-		#TODO code too slow...
-		if correct_index in np.argsort(-scores)[0,:5]:
+
+		if correct_index in np.argsort(-scores.data.numpy())[0,:5]:
 			TOP5 += 1
 
-	return TOP1, TOP5, len(data), TOP1/len(data)
+	return TOP1, TOP5, TOP1/len(data), TOP5/len(data), len(data)
 
 
 #Creates a class NaiveCBOW 
@@ -56,37 +45,33 @@ class NaiveCBOW(nn.Module):
 	#output_dim should be one according to the current dimention
 	def __init__(self, vocab_size, embedding_dim, img_feature_size, output_dim):
 		super(NaiveCBOW, self).__init__()
-		self.n_embedding = embedding_dim
-		self.img_feature_size = img_feature_size
-		self.output_dim = output_dim
 		self.embeddings = nn.Embedding(vocab_size, embedding_dim)
 		#TODO check if nn.Parameter or nn.Linear
-		self.linear = nn.Linear(embedding_dim + img_feature_size, 1)
+		self.linear = nn.Linear((embedding_dim + img_feature_size), 1)
+		self.activation_function = nn.Softmax()
 
 		#Inputs should be in Tensor form
 		#image_input is list of tensors
 	def forward(self, text_input, image_input):
 		embeds = self.embeddings(text_input)
 		sum_embeds = torch.sum(embeds, 1)
-		#matrix = make_matrix(sum_embeds, image_input, self.n_embedding, self.img_feature_size, self.output_dim)
-		#outputs = self.linear(matrix)
-		output = Variable(torch.zeros(1, self.output_dim))
-		i = 0
-		for image_feature in image_input:
-			conc = torch.cat((sum_embeds, image_feature),1)
-			result = self.linear(conc)
-			output[:,i] = result
-			i += 1
-
-		m = nn.Softmax()
-		output = m(output)
+		#create block matrix
+		sum_embeds_block = sum_embeds.repeat(10,1)
+		image_block = torch.stack(image_input).squeeze()
+		block = torch.cat((sum_embeds_block, image_block), 1)
+		
+		score = self.linear(block)
+		output = self.activation_function(score.view(1,-1))
+		
 		return output
 
+
 model = NaiveCBOW(nwords, 300, 2048, 10)
-#print (model)
+
 
 #TODO Check other learning rates
 optimizer = optim.Adam(model.parameters(), lr=0.01)
+loss = nn.CrossEntropyLoss()
 
 #First scoring on validation
 print ("Score on validation", evaluate(model, dev))
@@ -106,23 +91,23 @@ for ITER in range(10):
 		target = Variable(torch.LongTensor([correct_index]))
 
 		#Define loss
-		loss = nn.CrossEntropyLoss()
 		output = loss(score, target)
 		train_loss += output.data[0]
 
 		model.zero_grad()
 		output.backward()
+		#print (model.linear.grad)
 		optimizer.step()
 
 		if iteration % 100 == 0:
-			print("iter %r: %r: train loss/sent=%.4f, time=%.2fs" % (ITER, iteration, train_loss, time.time() - start))
+			print("iter %r: %r: train loss/sent=%.4f, time=%.2fs" % (ITER, iteration, train_loss/(iteration + 1), time.time() - start))
 	
 	print("iter %r: train loss/sent=%.4f, time=%.2fs" % (ITER, train_loss/len(train), time.time()-start))
 	print ("Score on validation", evaluate(model, dev))
 
 
-#TODO Save model
-
+#Save model
+torch.save(model, "Models/naive_cbow_easy.pty")
 
 
 
