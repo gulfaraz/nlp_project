358a0a52ea2628cb2a93c359a846b8e719042cea
Kishan Parshotam <kishanparshotam@msn.com>
2017-12-08 19:06:28 +0100

    tuning

--- nlp preprocessing/train.py
+++ nlp preprocessing/train.py
@@ -4,7 +4,6 @@ from model import NaiveCBOW
 from evaluate import Evaluate
 
 #Import other modules/libraries
-import sys
 import time
 import torch
 import random
@@ -18,117 +17,125 @@ import matplotlib.pyplot as plt
 
 torch.manual_seed(42)
 
-difficulty = "easy"
+class Train():
+    def __init__(self, difficulty):
+        self.data_path = "../data"
+        self.model_path = "../models"
+        self.output_path = "../outputs"
+        self.difficulty = difficulty
+        self.timestamp = str(int(time.time()))
+        self.model_name = "nlp_preprocessing_" + self.difficulty
+        self.data = Data(difficulty=self.difficulty, data_path=self.data_path)
+        (self.img_features, self.w2i, self.i2w, self.nwords, self.UNK, self.PAD) = self.data()
+        self.train = list(self.data.get_train_data())
+        self.dev = list(self.data.get_validation_data())
+        self.test = list(self.data.get_test_data())
+        self.image_feature_size = 2048
+        self.output_vector_size = 10
 
-if len(sys.argv) > 1:
-    difficulty = sys.argv[1]
+    def __call__(self, number_of_iterations = 2, learning_rate = 0.005, embedding_size = 300):
+        print("Starting 'Image Retrieval' in 'NLP Preprocessing' mode with '" + self.difficulty + "' data")
 
-if difficulty != "hard":
-    difficulty = "easy"
+        self.model_full_path = self.model_path + "/" + self.model_name + "_" + self.timestamp + "_" + str(learning_rate) + "_" + str(embedding_size) + ".pty"
+        self.output_file_name = self.output_path + "/" + self.model_name + "_" + self.timestamp + "_" + str(learning_rate) + "_" + str(embedding_size) + ".csv"
 
-data_path = "../data"
-model_path = "../models"
+        self.number_of_iterations = number_of_iterations
+        self.learning_rate = learning_rate
+        self.embedding_size = embedding_size
+        self.model = NaiveCBOW(self.nwords, self.embedding_size, self.image_feature_size, self.output_vector_size)
+        self.criterion = nn.CrossEntropyLoss()
 
-print("Starting 'Image Retrieval' in 'NLP Preprocessing' mode with '" + difficulty + "' data")
-model_name = "naive_cbow_" + difficulty
+        self.evaluate = Evaluate(self.model, self.img_features, self.minibatch, self.preprocess, self.image_feature_size, self.output_vector_size)
+        print(self.model)
 
-data = Data(difficulty=difficulty, data_path=data_path)
-(img_features, w2i, i2w, nwords, UNK, PAD) = data()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
 
-#Creates a list with word indexes, list of all target image indexes, and tag_index 
-train = list(data.get_train_data())
-dev = list(data.get_validation_data())
-test = list(data.get_test_data())
+        self.train_loss_values = []
 
-def minibatch(data, batch_size = 5000):
-    for i in range(0, len(data), batch_size):
-        yield data[i:i+batch_size]
+        self.magic()
 
-def preprocess(batch):
-    """Helper function for functional batches"""
-    correct_indexes = [observation[2] for observation in batch]
-    img_ids = [observation[1] for observation in batch]
-    text_features = [observation[0] for observation in batch]
+        self.save_model()
 
-    #Add Padding to max len of sentence in batch
-    max_length = max(map(len, text_features))
-    text_features = [txt + [PAD] * (max_length - len(txt)) for txt in text_features]
+        self.save_data()
 
-    #return in "stacked" format 
-    return text_features, img_ids, correct_indexes
+    def minibatch(self, data, batch_size = 5000):
+        for i in range(0, len(data), batch_size):
+            yield data[i:i+batch_size]
 
-number_of_iterations = 10
-learning_rate = 0.005
-embedding_size = 300
-image_feature_size = 2048
-output_vector_size = 10
-verbosity_interval = 1000
+    def preprocess(self, batch):
+        """Helper function for functional batches"""
+        correct_indexes = [observation[2] for observation in batch]
+        img_ids = [observation[1] for observation in batch]
+        text_features = [observation[0] for observation in batch]
 
-model = NaiveCBOW(nwords, embedding_size, image_feature_size, output_vector_size)
-criterion = nn.CrossEntropyLoss()
+        #Add Padding to max len of sentence in batch
+        max_length = max(map(len, text_features))
+        text_features = [txt + [self.PAD] * (max_length - len(txt)) for txt in text_features]
 
-evaluate = Evaluate(model, img_features, minibatch, preprocess, image_feature_size, output_vector_size)
-print(model)
+        #return in "stacked" format 
+        return text_features, img_ids, correct_indexes
 
-#TODO Check other learning rates
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    def magic(self):
+        for ITER in range(self.number_of_iterations):
 
-train_loss_values = []
+            random.shuffle(self.train)
+            train_loss = 0.0
+            start = time.time()
 
-#Initial scoring
-print("Score on training", evaluate(train))
-print("Score on development", evaluate(dev))
+            for batch_iteration, batch in enumerate(self.minibatch(self.train)):
 
-for ITER in range(number_of_iterations):
+                self.model.zero_grad()
+                self.optimizer.zero_grad()
 
-    random.shuffle(train)
-    train_loss = 0.0
-    start = time.time()
+                #Data for the model
+                text_features, h5_ids, correct_index = self.preprocess(batch)
+                lookup_text_tensor = Variable(torch.LongTensor([text_features])).squeeze()
 
-    for batch_iteration, batch in enumerate(minibatch(train)):
+                full_img_batch = np.empty([len(batch), self.output_vector_size, self.image_feature_size])
 
-        model.zero_grad()
-        optimizer.zero_grad()
+                for obs, img_ids in enumerate(h5_ids):
+                    for index, h5_id in enumerate(img_ids):
+                        full_img_batch[obs, index] = self.img_features[h5_id]
+                    
+                full_img_batch = Variable(torch.from_numpy(full_img_batch).type(torch.FloatTensor))
 
-        #Data for the model
-        text_features, h5_ids, correct_index = preprocess(batch)
-        lookup_text_tensor = Variable(torch.LongTensor([text_features])).squeeze()
+                #Target
+                target = Variable(torch.LongTensor([correct_index])).squeeze()
 
-        full_img_batch = np.empty([len(batch), output_vector_size, image_feature_size])
+                #Run model and calculate loss
+                prediction = self.model(lookup_text_tensor, full_img_batch)
+                
+                loss = self.criterion(prediction, target)
+                train_loss += loss.data[0]
 
-        for obs, img_ids in enumerate(h5_ids):
-            for index, h5_id in enumerate(img_ids):
-                full_img_batch[obs, index] = img_features[h5_id]
+                loss.backward()
+                self.optimizer.step()
+
+                #if iteration % verbosity_interval == 0 and iteration != 0:
+                #   print("ITERATION %r: %r: train loss/sent=%.4f, time=%.2fs" % (ITER+1, iteration, train_loss/(iteration + 1), time.time() - start))
             
-        full_img_batch = Variable(torch.from_numpy(full_img_batch).type(torch.FloatTensor))
+            print("ITERATION %r: train loss/sent=%.4f, time=%.2fs" % (ITER+1, train_loss/len(self.train), time.time() - start))
+            #print("Score on training", evaluate(train))
+            #print("Score on development", evaluate(dev))
+            self.train_loss_values.append(train_loss/len(self.train))
 
-        #Target
-        target = Variable(torch.LongTensor([correct_index])).squeeze()
+    def save_model(self):
+        #Save model
+        torch.save(self.model, self.model_full_path)
+        print("Saved model has test score", self.evaluate(self.test))
 
-        #Run model and calculate loss
-        prediction = model(lookup_text_tensor, full_img_batch)
-        
-        loss = criterion(prediction, target)
-        train_loss += loss.data[0]
+    def plot(self):
+        plt.plot(self.train_loss_values, label = "Train loss")
+        plt.legend(loc='best')
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss")
+        plt.title(self.model_name + " - has loss with lr = %.4f, embedding size = %r" % (self.learning_rate, self.embedding_size))
+        plt.show()
 
-        loss.backward()
-        optimizer.step()
-
-        #if iteration % verbosity_interval == 0 and iteration != 0:
-        #   print("ITERATION %r: %r: train loss/sent=%.4f, time=%.2fs" % (ITER+1, iteration, train_loss/(iteration + 1), time.time() - start))
-    
-    print("ITERATION %r: train loss/sent=%.4f, time=%.2fs" % (ITER+1, train_loss/len(train), time.time() - start))
-    print("Score on training", evaluate(train))
-    print("Score on development", evaluate(dev))
-    train_loss_values.append(train_loss/len(train))
-
-#Save model
-torch.save(model, model_path + "/" + model_name + ".pty")
-print("Saved model has test score", evaluate(test))
-
-plt.plot(train_loss_values, label = "Train loss")
-plt.legend(loc='best')
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
-plt.title (model_name + " - has loss with lr = %.4f, embedding size = %r" % (learning_rate, embedding_size))
-plt.show()
+    def save_data(self):
+        file = open(self.output_file_name, "w")
+        file.write(", ".join(map(str, self.train_loss_values)))
+        file.write("\n")
+        file.write(str(self.evaluate(self.test)))
+        file.write("\n")
+        file.close()
